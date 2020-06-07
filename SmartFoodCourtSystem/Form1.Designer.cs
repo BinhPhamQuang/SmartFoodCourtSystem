dcef36cbb6304ade234b18fe18ac437ac28766fd
Binh Pham Quang <bnh.quang@gmail.com>
2020-06-07 17:14:43 +0700

    Create Usercontrol

--- SmartFoodCourtSystem/Form1.Designer.cs
+++ SmartFoodCourtSystem/Form1.Designer.cs
@@ -30,23 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnDelivery = new System.Windows.Forms.Button();
+            this.bntPaymentMethod = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCart = new System.Windows.Forms.Button();
+            this.bntMenu = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnHome = new System.Windows.Forms.Button();
-            this.bntMenu = new System.Windows.Forms.Button();
-            this.btnCart = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.bntPaymentMethod = new System.Windows.Forms.Button();
-            this.btnDelivery = new System.Windows.Forms.Button();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnSetting = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.userControl_Home1 = new SmartFoodCourtSystem.UserControl_Home();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -63,128 +66,16 @@
             this.panel1.ForeColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(188, 536);
+            this.panel1.Size = new System.Drawing.Size(188, 570);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // panel4
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(188, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(837, 10);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Controls.Add(this.pictureBox1);
-            this.panel3.Location = new System.Drawing.Point(206, 9);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(106, 136);
-            this.panel3.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(16, 8);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(74, 71);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(1, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 21);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Smart Foods";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(17, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "restaurant";
-            // 
-            // btnHome
-            // 
-            this.btnHome.FlatAppearance.BorderSize = 0;
-            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHome.ForeColor = System.Drawing.Color.White;
-            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
-            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnHome.Location = new System.Drawing.Point(0, 61);
-            this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(187, 68);
-            this.btnHome.TabIndex = 0;
-            this.btnHome.Text = "Home";
-            this.btnHome.UseVisualStyleBackColor = true;
-            // 
-            // bntMenu
-            // 
-            this.bntMenu.FlatAppearance.BorderSize = 0;
-            this.bntMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bntMenu.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bntMenu.ForeColor = System.Drawing.Color.White;
-            this.bntMenu.Image = ((System.Drawing.Image)(resources.GetObject("bntMenu.Image")));
-            this.bntMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bntMenu.Location = new System.Drawing.Point(0, 135);
-            this.bntMenu.Name = "bntMenu";
-            this.bntMenu.Size = new System.Drawing.Size(187, 68);
-            this.bntMenu.TabIndex = 1;
-            this.bntMenu.Text = "Menu";
-            this.bntMenu.UseVisualStyleBackColor = true;
-            // 
-            // btnCart
-            // 
-            this.btnCart.FlatAppearance.BorderSize = 0;
-            this.btnCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCart.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCart.ForeColor = System.Drawing.Color.White;
-            this.btnCart.Image = ((System.Drawing.Image)(resources.GetObject("btnCart.Image")));
-            this.btnCart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCart.Location = new System.Drawing.Point(0, 209);
-            this.btnCart.Name = "btnCart";
-            this.btnCart.Size = new System.Drawing.Size(187, 68);
-            this.btnCart.TabIndex = 2;
-            this.btnCart.Text = "Cart";
-            this.btnCart.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 514);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Version 1.0";
-            // 
-            // bntPaymentMethod
-            // 
-            this.bntPaymentMethod.FlatAppearance.BorderSize = 0;
-            this.bntPaymentMethod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bntPaymentMethod.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bntPaymentMethod.ForeColor = System.Drawing.Color.White;
-            this.bntPaymentMethod.Image = ((System.Drawing.Image)(resources.GetObject("bntPaymentMethod.Image")));
-            this.bntPaymentMethod.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bntPaymentMethod.Location = new System.Drawing.Point(1, 357);
-            this.bntPaymentMethod.Name = "bntPaymentMethod";
-            this.bntPaymentMethod.Size = new System.Drawing.Size(187, 68);
-            this.bntPaymentMethod.TabIndex = 4;
-            this.bntPaymentMethod.Text = "Payment \r\nmethod";
-            this.bntPaymentMethod.UseVisualStyleBackColor = true;
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel4.Location = new System.Drawing.Point(178, 61);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 68);
+            this.panel4.TabIndex = 3;
             // 
             // btnDelivery
             // 
@@ -201,13 +92,125 @@
             this.btnDelivery.Text = "Delivery";
             this.btnDelivery.UseVisualStyleBackColor = true;
             // 
-            // panel4
+            // bntPaymentMethod
             // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel4.Location = new System.Drawing.Point(178, 61);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(10, 68);
-            this.panel4.TabIndex = 3;
+            this.bntPaymentMethod.FlatAppearance.BorderSize = 0;
+            this.bntPaymentMethod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntPaymentMethod.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntPaymentMethod.ForeColor = System.Drawing.Color.White;
+            this.bntPaymentMethod.Image = ((System.Drawing.Image)(resources.GetObject("bntPaymentMethod.Image")));
+            this.bntPaymentMethod.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bntPaymentMethod.Location = new System.Drawing.Point(1, 357);
+            this.bntPaymentMethod.Name = "bntPaymentMethod";
+            this.bntPaymentMethod.Size = new System.Drawing.Size(187, 68);
+            this.bntPaymentMethod.TabIndex = 4;
+            this.bntPaymentMethod.Text = "Payment \r\nmethod";
+            this.bntPaymentMethod.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 548);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Version 1.0";
+            // 
+            // btnCart
+            // 
+            this.btnCart.FlatAppearance.BorderSize = 0;
+            this.btnCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCart.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCart.ForeColor = System.Drawing.Color.White;
+            this.btnCart.Image = ((System.Drawing.Image)(resources.GetObject("btnCart.Image")));
+            this.btnCart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCart.Location = new System.Drawing.Point(0, 209);
+            this.btnCart.Name = "btnCart";
+            this.btnCart.Size = new System.Drawing.Size(187, 68);
+            this.btnCart.TabIndex = 2;
+            this.btnCart.Text = "Cart";
+            this.btnCart.UseVisualStyleBackColor = true;
+            // 
+            // bntMenu
+            // 
+            this.bntMenu.FlatAppearance.BorderSize = 0;
+            this.bntMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntMenu.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntMenu.ForeColor = System.Drawing.Color.White;
+            this.bntMenu.Image = ((System.Drawing.Image)(resources.GetObject("bntMenu.Image")));
+            this.bntMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bntMenu.Location = new System.Drawing.Point(0, 135);
+            this.bntMenu.Name = "bntMenu";
+            this.bntMenu.Size = new System.Drawing.Size(187, 68);
+            this.bntMenu.TabIndex = 1;
+            this.bntMenu.Text = "Menu";
+            this.bntMenu.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(0, 61);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(187, 68);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(188, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(841, 10);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(206, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(106, 136);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "restaurant";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(1, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Smart Foods";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // btnExit
             // 
@@ -238,16 +241,34 @@
             this.btnSetting.TabIndex = 7;
             this.btnSetting.UseVisualStyleBackColor = true;
             // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.userControl_Home1);
+            this.panel5.Location = new System.Drawing.Point(211, 132);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(802, 429);
+            this.panel5.TabIndex = 8;
+            // 
+            // userControl_Home1
+            // 
+            this.userControl_Home1.BackColor = System.Drawing.Color.White;
+            this.userControl_Home1.Location = new System.Drawing.Point(15, 15);
+            this.userControl_Home1.Name = "userControl_Home1";
+            this.userControl_Home1.Size = new System.Drawing.Size(763, 411);
+            this.userControl_Home1.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1025, 536);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1029, 570);
             this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -258,6 +279,7 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -279,6 +301,8 @@
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Panel panel5;
+        private UserControl_Home userControl_Home1;
     }
 }
 
---
+++ SmartFoodCourtSystem/UserControl_Home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartFoodCourtSystem
+{
+    public partial class UserControl_Home : UserControl
+    {
+        public UserControl_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Home_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
