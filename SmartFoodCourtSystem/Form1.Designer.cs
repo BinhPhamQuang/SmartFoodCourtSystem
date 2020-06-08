57d7bdfb08724551faac35c9694666558a70b088
Binh Pham Quang <bnh.quang@gmail.com>
2020-06-08 21:34:22 +0700

    create Management project

--- SmartFoodCourtSystem/Form1.Designer.cs
+++ SmartFoodCourtSystem/Form1.Designer.cs
@@ -288,8 +288,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1029, 570);
-            this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
