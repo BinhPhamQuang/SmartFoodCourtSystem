4c2ede5e87ebf6fcb21609cf38ffcf2860f7a262
BuiLeQuocHung <66366262+BuiLeQuocHung@users.noreply.github.com>
2020-06-14 23:44:23 +0700

    Add files via upload

---
+++ Start.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SmartFoodCourtSystem
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.LoginHome = new System.Windows.Forms.Button();
+            this.RegisterHome = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-2, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(443, 121);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(90, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 38);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Smart FoodCourt";
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.BtnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnExit.BackgroundImage")));
+            this.BtnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.BtnExit.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnExit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BtnExit.Location = new System.Drawing.Point(394, 0);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(47, 41);
+            this.BtnExit.TabIndex = 26;
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // panelControls
+            // 
+            this.panelControls.Location = new System.Drawing.Point(-2, 307);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(443, 268);
+            this.panelControls.TabIndex = 27;
+            // 
+            // LoginHome
+            // 
+            this.LoginHome.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.LoginHome.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginHome.Location = new System.Drawing.Point(-2, 227);
+            this.LoginHome.Name = "LoginHome";
+            this.LoginHome.Size = new System.Drawing.Size(221, 43);
+            this.LoginHome.TabIndex = 28;
+            this.LoginHome.Text = "Login";
+            this.LoginHome.UseVisualStyleBackColor = false;
+            this.LoginHome.Click += new System.EventHandler(this.LoginHome_Click);
+            // 
+            // RegisterHome
+            // 
+            this.RegisterHome.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.RegisterHome.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterHome.Location = new System.Drawing.Point(220, 227);
+            this.RegisterHome.Name = "RegisterHome";
+            this.RegisterHome.Size = new System.Drawing.Size(221, 43);
+            this.RegisterHome.TabIndex = 28;
+            this.RegisterHome.Text = "Register";
+            this.RegisterHome.UseVisualStyleBackColor = false;
+            this.RegisterHome.Click += new System.EventHandler(this.RegisterHome_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(442, 576);
+            this.Controls.Add(this.RegisterHome);
+            this.Controls.Add(this.LoginHome);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.Button LoginHome;
+        private System.Windows.Forms.Button RegisterHome;
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,50 @@
+﻿using SmartFoodCourtSystem.UC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartFoodCourtSystem
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+            Login login = new Login();
+            AddControlsToPanel(login);
+        }
+
+
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+        void AddControlsToPanel(Control c)
+        {
+            c.Dock = DockStyle.Fill;
+            panelControls.Controls.Clear();
+            panelControls.Controls.Add(c);
+        }
+
+
+
+        private void LoginHome_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            AddControlsToPanel(login);
+        }
+
+        private void RegisterHome_Click(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            AddControlsToPanel(register);
+        }
+    }
+}
