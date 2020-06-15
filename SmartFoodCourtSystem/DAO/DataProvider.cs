33fe53322ee4952850d9a28b1bec67b49cececd8
Binh Pham Quang <bnh.quang@gmail.com>
2020-06-15 21:38:34 +0700

    Edit class DataProvider

--- SmartFoodCourtSystem/DAO/DataProvider.cs
+++ SmartFoodCourtSystem/DAO/DataProvider.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
 namespace SmartFoodCourtSystem.DAO
 {
     class DataProvider
     {
+        private static DataProvider instance;
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
+            private set { DataProvider.instance = value; }
+        }
+        private DataProvider() { }
+        private string connectionSTR = "Data Source=database.db;Version=3;New=False;Compress=True;";
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SQLiteConnection sql_conn = new SQLiteConnection(connectionSTR))
+            {
+                SQLiteCommand cmd = new SQLiteCommand(query, sql_conn);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SQLiteDataAdapter adr = new SQLiteDataAdapter(cmd);
+                adr.Fill(data);
+                sql_conn.Close();
+            }
+            return data;
+        }
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            return data;
+        }
     }
 }
