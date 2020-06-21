5d88a9192e30bb12714dd8ac325b158b7d1a3f91
BuiLeQuocHung <quochungbuile@gmail.com>
2020-06-21 11:25:22 +0700

    change design cook

--- SmartFoodCourtSystem/Cook.Designer.cs
+++ SmartFoodCourtSystem/Cook.Designer.cs
@@ -159,7 +159,7 @@
             this.dataGridView2.Location = new System.Drawing.Point(0, 0);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
-            this.dataGridView2.Size = new System.Drawing.Size(831, 396);
+            this.dataGridView2.Size = new System.Drawing.Size(834, 396);
             this.dataGridView2.TabIndex = 0;
             this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
