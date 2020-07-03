378e8bfdf0f741a08f35c58d0ab37b6974c30c1f
andy <qd24052000@gmail.com>
2020-07-03 18:53:48 +0700

    Change revenue report

--- SmartFoodCourtSystem/UCReport.Designer.cs
+++ SmartFoodCourtSystem/UCReport.Designer.cs
@@ -40,16 +40,26 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.cb3 = new System.Windows.Forms.ComboBox();
             this.cb2 = new System.Windows.Forms.ComboBox();
-            this.cb1 = new System.Windows.Forms.ComboBox();
             this.btnMakeReport = new System.Windows.Forms.Button();
             this.grBox = new System.Windows.Forms.GroupBox();
             this.btnExit = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.lV2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lV1 = new System.Windows.Forms.ListView();
+            this.Month = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SalesRevenue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CostOfSales = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.GrossProfit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.grBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
@@ -126,21 +136,19 @@
             this.groupBox2.BackColor = System.Drawing.Color.Transparent;
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.cb3);
             this.groupBox2.Controls.Add(this.cb2);
-            this.groupBox2.Controls.Add(this.cb1);
             this.groupBox2.Controls.Add(this.btnMakeReport);
             this.groupBox2.Location = new System.Drawing.Point(6, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(431, 98);
+            this.groupBox2.Size = new System.Drawing.Size(339, 98);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(170, 23);
+            this.label3.Location = new System.Drawing.Point(84, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 16;
@@ -149,20 +157,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(94, 23);
+            this.label2.Location = new System.Drawing.Point(6, 23);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 17);
             this.label2.TabIndex = 15;
             this.label2.Text = "Month";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 17);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Day";
             // 
             // cb3
             // 
@@ -174,7 +173,7 @@
             "2017",
             "2016",
             "2015"});
-            this.cb3.Location = new System.Drawing.Point(173, 43);
+            this.cb3.Location = new System.Drawing.Point(85, 43);
             this.cb3.Name = "cb3";
             this.cb3.Size = new System.Drawing.Size(70, 24);
             this.cb3.TabIndex = 13;
@@ -195,57 +194,17 @@
             "10",
             "11",
             "12"});
-            this.cb2.Location = new System.Drawing.Point(97, 43);
+            this.cb2.Location = new System.Drawing.Point(9, 43);
             this.cb2.Name = "cb2";
             this.cb2.Size = new System.Drawing.Size(70, 24);
             this.cb2.TabIndex = 12;
-            // 
-            // cb1
-            // 
-            this.cb1.FormattingEnabled = true;
-            this.cb1.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25",
-            "26",
-            "27",
-            "28",
-            "29",
-            "30",
-            "31"});
-            this.cb1.Location = new System.Drawing.Point(21, 43);
-            this.cb1.Name = "cb1";
-            this.cb1.Size = new System.Drawing.Size(70, 24);
-            this.cb1.TabIndex = 11;
             // 
             // btnMakeReport
             // 
             this.btnMakeReport.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnMakeReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMakeReport.ForeColor = System.Drawing.Color.White;
-            this.btnMakeReport.Location = new System.Drawing.Point(258, 31);
+            this.btnMakeReport.Location = new System.Drawing.Point(161, 31);
             this.btnMakeReport.Name = "btnMakeReport";
             this.btnMakeReport.Size = new System.Drawing.Size(167, 46);
             this.btnMakeReport.TabIndex = 10;
@@ -256,8 +215,11 @@
             // grBox
             // 
             this.grBox.BackColor = System.Drawing.Color.DarkGray;
+            this.grBox.Controls.Add(this.label1);
+            this.grBox.Controls.Add(this.btnExport);
+            this.grBox.Controls.Add(this.lV2);
+            this.grBox.Controls.Add(this.lV1);
             this.grBox.Controls.Add(this.btnExit);
-            this.grBox.Controls.Add(this.label5);
             this.grBox.Controls.Add(this.pictureBox1);
             this.grBox.Controls.Add(this.label4);
             this.grBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -281,22 +243,10 @@
             this.btnExit.TabStop = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Yellow;
-            this.label5.Location = new System.Drawing.Point(230, 286);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 36);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "label5";
-            this.label5.Visible = false;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-69, 130);
+            this.pictureBox1.Location = new System.Drawing.Point(331, -24);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(383, 237);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -314,6 +264,117 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "Chuỗi nhà hàng Hippo Town";
             this.label4.Visible = false;
+            // 
+            // lV2
+            // 
+            this.lV2.BackColor = System.Drawing.Color.White;
+            this.lV2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lV2.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lV2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lV2.FullRowSelect = true;
+            this.lV2.GridLines = true;
+            this.lV2.HideSelection = false;
+            this.lV2.Location = new System.Drawing.Point(264, 238);
+            this.lV2.Name = "lV2";
+            this.lV2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lV2.Scrollable = false;
+            this.lV2.Size = new System.Drawing.Size(656, 82);
+            this.lV2.TabIndex = 9;
+            this.lV2.UseCompatibleStateImageBehavior = false;
+            this.lV2.View = System.Windows.Forms.View.Details;
+            this.lV2.Visible = false;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Year";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Sales Revenue";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Cost Of Sales";
+            this.columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Gross Profit";
+            this.columnHeader5.Width = 150;
+            // 
+            // lV1
+            // 
+            this.lV1.BackColor = System.Drawing.Color.White;
+            this.lV1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Month,
+            this.Year,
+            this.SalesRevenue,
+            this.CostOfSales,
+            this.GrossProfit});
+            this.lV1.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lV1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lV1.FullRowSelect = true;
+            this.lV1.GridLines = true;
+            this.lV1.HideSelection = false;
+            this.lV1.Location = new System.Drawing.Point(232, 238);
+            this.lV1.Name = "lV1";
+            this.lV1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lV1.Scrollable = false;
+            this.lV1.Size = new System.Drawing.Size(718, 82);
+            this.lV1.TabIndex = 8;
+            this.lV1.UseCompatibleStateImageBehavior = false;
+            this.lV1.View = System.Windows.Forms.View.Details;
+            this.lV1.Visible = false;
+            // 
+            // Month
+            // 
+            this.Month.Text = "Month";
+            // 
+            // Year
+            // 
+            this.Year.Text = "Year";
+            // 
+            // SalesRevenue
+            // 
+            this.SalesRevenue.Text = "Sales Revenue";
+            this.SalesRevenue.Width = 150;
+            // 
+            // CostOfSales
+            // 
+            this.CostOfSales.Text = "Cost Of Sales";
+            this.CostOfSales.Width = 150;
+            // 
+            // GrossProfit
+            // 
+            this.GrossProfit.Text = "Gross Profit";
+            this.GrossProfit.Width = 150;
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.Gray;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.Yellow;
+            this.btnExport.Location = new System.Drawing.Point(500, 575);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(120, 48);
+            this.btnExport.TabIndex = 10;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 457);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
             // 
             // UCReport
             // 
@@ -351,14 +412,24 @@
         private System.Windows.Forms.Button btnMakeReport;
         private System.Windows.Forms.ComboBox cb3;
         private System.Windows.Forms.ComboBox cb2;
-        private System.Windows.Forms.ComboBox cb1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox grBox;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox btnExit;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ListView lV2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ListView lV1;
+        private System.Windows.Forms.ColumnHeader Month;
+        private System.Windows.Forms.ColumnHeader Year;
+        private System.Windows.Forms.ColumnHeader SalesRevenue;
+        private System.Windows.Forms.ColumnHeader CostOfSales;
+        private System.Windows.Forms.ColumnHeader GrossProfit;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SmartFoodCourtSystem/UCReport.cs
+++ SmartFoodCourtSystem/UCReport.cs
@@ -101,136 +101,116 @@ namespace SmartFoodCourtSystem
 
         private void btnMakeReport_Click(object sender, EventArgs e)
         {
-            int money = 0;
-            label5.Text = "Total revenue of ";
-            if (cb1.SelectedIndex > -1 && cb2.SelectedIndex > -1 && cb3.SelectedIndex > -1)
+            int type = -1;
+            int reve = 0;
+            int l_reve = 0;
+            string month = "";
+            string year = "";
+            double cost = 0.6742389 * reve;
+            double l_cost = 0.7742389 * l_reve;
+            ListViewItem a = new ListViewItem();
+            ListViewItem b = new ListViewItem();
+            //label5.Text = "Total revenue of ";
+            if (cb2.SelectedIndex > -1 && cb3.SelectedIndex > -1)
             {
-                string d = cb1.SelectedItem.ToString();
-                string y = cb3.SelectedItem.ToString();
-                string m = cb2.SelectedItem.ToString();
-                string query = $"SELECT * from Bill WHERE Date={d} AND Month ={m} AND Year ={y} ";
-                DataTable data = DataProvider.Instance.ExecuteQuery(query);
-                foreach (DataRow r in data.Rows)
-                {
-                    money += int.Parse(r["Totalprice"].ToString());
-                }
-                string thang = "";
-                switch (int.Parse(m))
-                {
-                    case 1:
-                        thang = "Jan";
-                        break;
-                    case 2:
-                        thang = "Feb";
-                        break;
-                    case 3:
-                        thang = "Mar";
-                        break;
-                    case 4:
-                        thang = "Apr";
-                        break;
-                    case 5:
-                        thang = "May";
-                        break;
-                    case 6:
-                        thang = "Jun";
-                        break;
-                    case 7:
-                        thang = "Jul";
-                        break;
-                    case 8:
-                        thang = "Aug";
-                        break;
-                    case 9:
-                        thang = "Sep";
-                        break;
-                    case 10:
-                        thang = "Oct";
-                        break;
-                    case 11:
-                        thang = "Nov";
-                        break;
-                    case 12:
-                        thang = "Dec";
-                        break;
-                }
-                label5.Text += d +" " + thang + ", " + y + " is " + changetoVND(money.ToString()) + " VND";
-            }
-            else if (cb1.SelectedIndex <= -1 && cb2.SelectedIndex > -1 && cb3.SelectedIndex > -1)
-            {
+                type = 0;
                 string y = cb3.SelectedItem.ToString();
                 string m = cb2.SelectedItem.ToString();
                 string query = $"SELECT * from Bill WHERE Month ={m} AND Year ={y} ";
                 DataTable data = DataProvider.Instance.ExecuteQuery(query);
+                month = m;
+                year = y;
                 foreach (DataRow r in data.Rows)
                 {
-                    money += int.Parse(r["Totalprice"].ToString());
+                    reve += int.Parse(r["Totalprice"].ToString());
                 }
-                string thang = "";
-                switch (int.Parse(m))
+                string query1 = $"SELECT * from Bill WHERE Month ={(int.Parse(m) - 1).ToString()} AND Year ={y} ";
+                DataTable data1 = DataProvider.Instance.ExecuteQuery(query1);
+                foreach (DataRow r in data1.Rows)
                 {
-                    case 1:
-                        thang = "Jan";
-                        break;
-                    case 2:
-                        thang = "Feb";
-                        break;
-                    case 3:
-                        thang = "Mar";
-                        break;
-                    case 4:
-                        thang = "Apr";
-                        break;
-                    case 5:
-                        thang = "May";
-                        break;
-                    case 6:
-                        thang = "Jun";
-                        break;
-                    case 7:
-                        thang = "Jul";
-                        break;
-                    case 8:
-                        thang = "Aug";
-                        break;
-                    case 9:
-                        thang = "Sep";
-                        break;
-                    case 10:
-                        thang = "Oct";
-                        break;
-                    case 11:
-                        thang = "Nov";
-                        break;
-                    case 12:
-                        thang = "Dec";
-                        break;
+                    l_reve += int.Parse(r["Totalprice"].ToString());
                 }
-                label5.Text += thang + ", " + y + " is " + changetoVND(money.ToString()) + " VND";
+                //label5.Text += thang + ", " + y + " is " + changetoVND(money.ToString()) + " VND";
             }
-            else if (cb1.SelectedIndex <= -1 && cb2.SelectedIndex <= -1 && cb3.SelectedIndex > -1)
+            else if (cb2.SelectedIndex <= -1 && cb3.SelectedIndex > -1)
             {
+                type = 1;
                 string y = cb3.SelectedItem.ToString();
                 string query = $"SELECT * from Bill WHERE Year ={y} ";
                 DataTable data = DataProvider.Instance.ExecuteQuery(query);
+                year = y;
                 foreach (DataRow r in data.Rows)
                 {
-                    money += int.Parse(r["Totalprice"].ToString());
+                    reve += int.Parse(r["Totalprice"].ToString());
                 }
-                label5.Text +=  y + " is " + changetoVND(money.ToString()) + " VND";
+                string query1 = $"SELECT * from Bill WHERE Year ={(int.Parse(y) - 1).ToString()}";
+                DataTable data1 = DataProvider.Instance.ExecuteQuery(query1);
+                foreach (DataRow r in data1.Rows)
+                {
+                    l_reve += int.Parse(r["Totalprice"].ToString());
+                }
+                //label5.Text +=  y + " is " + changetoVND(money.ToString()) + " VND";
             }
+            cost = Math.Round(0.55645645 * reve);
+            l_cost = Math.Round(0.75456456 * l_reve);
+            double grossprofit = 0;
+            double l_grossprofit = 0;
+            double percent = 0;
+            if (reve != 0)
+            {
+                grossprofit = (double)reve - cost;
+            }
+            if (l_reve != 0)
+            {
+                l_grossprofit = (double)l_reve - cost;
+                percent = Math.Round((grossprofit / l_grossprofit - 1), 2);
+            }
+            double diff = Math.Abs(grossprofit - l_grossprofit);
+            switch (type)
+            {
+                case 0:
+                    a.Text = (int.Parse(month) - 1).ToString();
+                    a.SubItems.Add(year);
+                    a.SubItems.Add(changetoVND(l_reve.ToString()) + " VND ");
+                    a.SubItems.Add(changetoVND(l_cost.ToString()) + " VND ");
+                    a.SubItems.Add(changetoVND(l_grossprofit.ToString()) + " VND ");
+                    b.Text = month;
+                    b.SubItems.Add(year);
+                    b.SubItems.Add(changetoVND(reve.ToString()) + " VND ");
+                    b.SubItems.Add(changetoVND(cost.ToString()) + " VND ");
+                    b.SubItems.Add(changetoVND(grossprofit.ToString()));
+                    lV1.Items.Add(a);
+                    lV1.Items.Add(b);
+                    lV1.Visible = true;
+                    if (grossprofit > l_grossprofit) label1.Text = "The gross profit of month " + month + " is greater than month " + (int.Parse(month) - 1).ToString() +" " + changetoVND(diff.ToString()) + "VND";
+                    else label1.Text = "The gross profit of month " + month + " is less than month " + (int.Parse(month) - 1).ToString() + " " + changetoVND(diff.ToString()) + "VND";
+                    break;
+                case 1:
+                    a.Text = (int.Parse(year) - 1).ToString();
+                    a.SubItems.Add(changetoVND(l_reve.ToString()) + " VND ");
+                    a.SubItems.Add(changetoVND(l_cost.ToString()) + " VND ");
+                    a.SubItems.Add(changetoVND(l_grossprofit.ToString()) + " VND ");
+                    b.Text = year;
+                    b.SubItems.Add(changetoVND(reve.ToString()) + " VND ");
+                    b.SubItems.Add(changetoVND(cost.ToString()) + " VND ");
+                    b.SubItems.Add(changetoVND(grossprofit.ToString()) + " VND ");
+                    lV2.Items.Add(a);
+                    lV2.Items.Add(b);
+                    lV2.Visible = true;
+                    if (grossprofit > l_grossprofit) label1.Text = "The gross profit of year " + year+ " is greater than year " + (int.Parse(year) - 1).ToString() + " " + changetoVND(diff.ToString()) + "VND";
+                    else label1.Text = "The gross profit of year " + year + " is less than year " + (int.Parse(year) - 1).ToString() + " " + changetoVND(diff.ToString()) + "VND";
+                    break;
+            }
+            
             grBox.BringToFront();
             grBox.Visible = true;
             label4.Visible = true;
-            label5.Visible = true;
-            if (money == 0) label5.Text = "No data found!";
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             grBox.Visible = false;
             label4.Visible = false;
-            label5.Visible = false;
         }
     }
 }
